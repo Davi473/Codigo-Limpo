ae51c90d09d01cc67701daa4039fc73bb25c8e8f
Davi473 <68740118+Davi473@users.noreply.github.com>
2023-12-05 03:25:38 +0000

    test2

--- atalho.bat
+++ atalho.bat
@@ -1,1 +1,4 @@
-node /workspaces/Codigo-Limpo/Servidor/server.js
+node /workspaces/Codigo-Limpo/test1/Servidor/server.js
+
+
+node /workspaces/Codigo-Limpo/test2/Servidor/server.js
---
+++ test2/public/js/web/web.js
@@ -0,0 +1,14 @@
+class Web {
+
+    constructor (site) {
+        this.site = site
+    }
+
+    async get(url) {
+
+    }
+
+    async post(url) {
+
+    }
+}
